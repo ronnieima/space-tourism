b00c3919a11f4a009de2a11422fafd92821d90e3
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2024-03-03 16:01:54 +0100

    work on home

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const links = [
+  { label: "home", href: "/" },
+  { label: "destination", href: "/" },
+  { label: "crew", href: "/" },
+  { label: "technology", href: "/" },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main className="h-screen bg-home-desktop bg-cover bg-center bg-no-repeat">
+      <nav className="absolute top-8 h-24 w-full">
+        <div className="flex h-full items-center">
+          <div className="bg-transparent">
+            <img src="/src/assets/shared/logo.svg" />
+          </div>
+          <div></div>
+          <li className="flex list-none text-white bg-blend-saturation ">
+            {links.map((link, i) => (
+              <a href={link.href}>
+                {i.toString().padStart(2, "0")}
+                {link.label}
+              </a>
+            ))}
+          </li>
+        </div>
+      </nav>
+    </main>
+  );
 }
 
-export default App
+export default App;
