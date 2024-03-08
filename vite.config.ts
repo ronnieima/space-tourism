b65f16480c4972734a02cd08430767b4a79f83b0
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2024-03-08 19:04:12 +0100

    delete unused assets

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), TanStackRouterVite()],
-  base: "./",
+  assetsInclude: ["**/*.jpg", "**/*.webp"],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
